6bdd92bd874d9ca05a3d36d59e0f7dabff50db19
NSantiago <nunezsantiago92@gmail.com>
2022-06-15 19:08:06 -0300

    posts list now refreshes and displays error msg

---
+++ src/components/ErrorScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const ErrorScreen = (): JSX.Element => {
+  return <Text>Can't load any post. Tap to reload</Text>;
+};
+
+export default ErrorScreen;
--- src/screens/SubredditScreen.tsx
+++ src/screens/SubredditScreen.tsx
@@ -1,7 +1,8 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useEffect, useState } from "react";
-import { FlatList, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { ActivityIndicator, FlatList, View } from "react-native";
 import { RootStackParams } from "../../App";
+import ErrorScreen from "../components/ErrorScreen";
 import PostListItem from "../components/PostListItem";
 import SortByMenu from "../components/SortByMenu";
 import { Post, SortBy } from "../model";
@@ -16,20 +17,59 @@ interface SubredditScreenProps {
   navigation: SubredditNavProps["navigation"];
   route: SubredditNavProps["route"];
 }
+type QueryStatus = "loading" | "error" | "success";
 
-const SubredditScreen = ({ navigation }: SubredditScreenProps): JSX.Element => {
+const SubredditScreen = ({
+  navigation,
+  route,
+}: SubredditScreenProps): JSX.Element => {
+  const { subreddit } = route.params;
+  const [sortBy, setSortBy] = useState<SortBy>("hot");
   const [posts, setPosts] = useState<Post[] | null>(null);
-  const [sortBy, setSortBy] = useState<SortBy>("new");
+  const [status, setStatus] = useState<QueryStatus>("loading");
+  const refreshing = status === "loading";
+  const isMounted = useRef<boolean>(true);
+
+  const loadPosts = () => {
+    setStatus("loading");
+    getPosts(sortBy, subreddit)
+      .then((posts) => {
+        if (posts.length === 0) throw new Error();
+        if (isMounted.current) {
+          setPosts(posts);
+          setStatus("success");
+        }
+      })
+      .catch((_) => {
+        if (isMounted.current) {
+          setStatus("error");
+        }
+      });
+  };
+
   useEffect(() => {
-    getPosts(sortBy, "pics").then((p) => {
-      setPosts(p);
-    });
-  }, [sortBy]);
+    isMounted.current = true;
+    loadPosts();
+    return () => {
+      isMounted.current = false;
+    };
+  }, [sortBy, subreddit]);
+
+  if (status === "loading")
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  if (status === "error") return <ErrorScreen />;
+
   return (
     <View>
       {posts && (
         <FlatList
           data={posts}
+          onRefresh={() => loadPosts()}
+          refreshing={refreshing}
           ListHeaderComponent={
             <SortByMenu sortBy={sortBy} setSortBy={setSortBy} />
           }
